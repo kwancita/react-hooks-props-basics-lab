f1b80ebaa7352164c56e3c59e510c4dc629022cb
Wasanta <kwan_u_day@hotmail.com>
2022-01-30 19:09:59 -0500

    commit tree 719a4df00852b637edac007bf14d9edcbcdb2580

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About({bio, link}) {
+  if(!bio){
+    return null
+  }else{
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+        <Links links = {link}/>
+      </div>
+    );
+  }
 }
 
 export default About;
